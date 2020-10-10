e4565a77d8ff33abb4596469671abf8e30efaad6
Simran Jariwala <simranjariwala@simrans-mbp.myfiosgateway.com>
2020-10-10 15:47:14 -0400

    Review by Simran Jariwala

--- Assignment1.py
+++ Assignment1.py
@@ -6,13 +6,15 @@ import matplotlib.pyplot as mp
 x = np.linspace(0, 2*np.pi, 100)
 y = np.sin(x)
 z = np.cos(x)
-
+t = np.tan(x)
 mp.plot(x, y, x, z)
+mp.plot(x,t)
+mp.ylim(-3,3)
 mp.xlabel('x')
 mp.ylabel('Function value')
 mp.title('Sine and Cosine functions')
 mp.grid()
 mp.show()
 
-
+##### Review by Simran : code looks good!I suggest you can also add a legend here. Review 5/5
 
